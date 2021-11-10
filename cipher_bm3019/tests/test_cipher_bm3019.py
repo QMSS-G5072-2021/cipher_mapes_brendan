3f1dd9523e007bd978eaeb1b095df87d51be580e
Brendan Mapes <75643602+brendanmapes@users.noreply.github.com>
2021-11-10 09:45:28 -0500

    Add files via upload

--- cipher_bm3019/tests/test_cipher_bm3019.py
+++ cipher_bm3019/tests/test_cipher_bm3019.py
@@ -1,1 +1,10 @@
-from cipher_bm3019 import cipher_bm3019
+import py.test
+from cipher_bm3019.cipher_bm3019 import cipher
+
+def test_for_word():
+    example = 'test'
+    expected = 'uftu'
+    actual = cipher(example, 1)
+    assert actual == expected
+        
+
